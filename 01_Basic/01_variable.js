e7caf743686b91de72d344f07f88bc477df7af00
varshasingh77 <74130445+varshasingh77@users.noreply.github.com>
2024-03-05 13:19:58 +0000

    Using variables

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 1354
+let accountEmail = "varsha@gmail.com"
+var accountPassword = "12345"
+
+console.log(accountId);
+
+accountEmail = "varsha123@vs.com"
+accountPassword = "123456"
+accountCity = "Delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity ])
