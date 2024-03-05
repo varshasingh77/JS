e3af90a71f2fb736a84a941446608dcca7ab47f7
varshasingh77 <74130445+varshasingh77@users.noreply.github.com>
2024-03-05 13:53:22 +0000

    datatypes

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "Varsha"
+let age = 19
+let isLoggedIn = false
+let state;
+
+//number
+//bigint
+//String=""
+//boolean=> true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+//object
+
+console.log(typeof "Varsha");
